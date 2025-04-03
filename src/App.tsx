b818d5749f2bf6916ef4fc8fd3728764c0a8e2ee
Mahesh Tammineni <maheshtammineni2222@gmail.com>
2025-04-03 16:20:06 +0530

    updated Ui added useContext and icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,79 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import './App.css'
 import ChatApp from './ChatApp'
+import { ThemeProvider } from './ThemeContext';
 
 function App() {
-
   return (
     <>
-   <ChatApp />
+        <ThemeProvider>
+        <ChatApp />
+        </ThemeProvider>
     </>
   )
 }
 
 export default App
+
+
+// import React from "react";
+// import { ThemeProvider } from "./ThemeContext";
+// import ThemeToggle from "./ThemeToggle";
+
+// const App = () => {
+//   return (
+//     <ThemeProvider>
+//       <ThemeToggle />
+//     </ThemeProvider>
+//   );
+// };
+
+// export default App;
+
+
+// import React, { useState, useEffect } from "react";
+// import io from "socket.io-client";
+
+// const socket = io.connect("http://localhost:5000");
+
+// const App = () => {
+//   const [message, setMessage] = useState("");
+//   const [chat, setChat] = useState([]);
+
+//   useEffect(() => {
+//     socket.on("receiveMessage", (message:any) => {
+//       setChat((prevChat) => [...prevChat, message] as any);
+//     });
+
+//     return () => {
+//       socket.off("receiveMessage");
+//     };
+//   }, []);
+
+//   const sendMessage = () => {
+//     if (message.trim()) {
+//       socket.emit("sendMessage", message);
+//       setMessage("");
+//     }
+//   };
+
+//   return (
+//     <div style={{ textAlign: "center", marginTop: "50px" }}>
+//       <h2>💬 Real-time Chat</h2>
+//       <div>
+//         {chat.map((msg, index) => (
+//           <p key={index}>{msg}</p>
+//         ))}
+//       </div>
+//       <input
+//         type="text"
+//         value={message}
+//         onChange={(e) => setMessage(e.target.value)}
+//         placeholder="Type a message..."
+//       />
+//       <button onClick={sendMessage}>Send</button>
+//     </div>
+//   );
+// };
+
+// export default App;
--- src/ChatApp.tsx
+++ src/ChatApp.tsx
@@ -1,49 +1,196 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { useEffect, useRef, useState } from "react"
+import { useEffect, useRef, useState , useContext } from "react";
+import sendIconImg from "../src/assets/icon-send.png";
+import personImage from "../src/assets/personImage.jpeg";
+import chatBotImage from "../src/assets/chatBotImage.png"; 
+import { ThemeContext } from "./ThemeContext";
+import { FaToggleOn , FaToggleOff  } from "react-icons/fa";
 
 const ChatApp = () => {
-    const [input, setinput] = useState("");
-    const [messages, setMessage] = useState([]);
-    const socketRef : any = useRef(null);
+  const [input, setinput] = useState("");
+  const [chat , setChat] = useState([]) as any;
+  const socketRef: any = useRef(null);
+  const { theme, toggleTheme } = useContext(ThemeContext) as any;
 
-    useEffect(()=> {
-     socketRef.current = new WebSocket('https://chatbotserver-production-ef2.up.railway.app/')
-     socketRef.current.onmessage = (event: any)=> {
-        setMessage((prev) =>[...prev,{type: 'bot', text: event.data}] as any)
-     }
+  useEffect(() => {
+    const savedChat =
+      JSON.parse(localStorage.getItem("chatHistory") as any) || [];
+    setChat(savedChat);
+  }, []);
 
-     return () => {
-        socketRef.current.close();
-     }
-    }, [])
+  useEffect(() => {
+    socketRef.current = new WebSocket('https://chatbotserver-production-ef2.up.railway.app/')
+    socketRef.current.onmessage = (event: any) => {
+      setChat((prev: any) => {
+        const now = new Date();
+        const time = now.toLocaleTimeString("en-US", {
+          hour: "2-digit",
+          minute: "2-digit",
+          hour12: false,
+        });
+        const weekday = now.toLocaleDateString("en-US", { weekday: "long" });
+        const updatedChat: any = [
+          ...prev,
+          { type: "bot", text: event.data, timestamp: time, weekday: weekday },
+        ];
 
-    const sendMessage = () => {
-        if(input.trim()){
-            socketRef.current.send(input);
-            setMessage((prev) => [...prev,{type: 'user', text: input}] as any)
-            setinput("")
-        }
+        localStorage.setItem("chatHistory", JSON.stringify(updatedChat));
+        return updatedChat;
+      });
+    };
+
+    return () => {
+      socketRef.current.close();
+    };
+  }, []);
+
+  const sendMessage = () => {
+    if (input.trim()) {
+      socketRef.current.send(input);
+      setChat((prev: any) => {
+        const now = new Date();
+        const time = now.toLocaleTimeString("en-US", {
+          hour: "2-digit",
+          minute: "2-digit",
+          hour12: false,
+        });
+        const weekday = now.toLocaleDateString("en-US", { weekday: "long" });
+        const updatedChat: any = [
+          ...prev,
+          { type: "user", text: input, timestamp: time, weekday: weekday },
+        ];
+
+        localStorage.setItem("chatHistory", JSON.stringify(updatedChat));
+        return updatedChat;
+      });
+      setinput("");
     }
+  };
 
-    return(
-        <>
-        <h2>Chat BOT App</h2>
-        <div style={{height: 500, width: 400, overflowY: 'scroll', border: '1px solid #ccc', padding: "10px"}}>
-           { messages.map((msg:any, index) => {
-            return(
-                <div style={{ textAlign: msg.type === "user" ? "right" : "left"}} key={index}>
-                <span style={{backgroundColor: "lightgreen", border: '1px solid gray', borderRadius: 10}}>
-                <strong>{msg.type === "user"? "YOU": "BOT"}:</strong>{msg.text}
+  return (
+    <>
+      <h2>
+        <span style={{ backgroundColor: "green" }}>💬</span> Real-time Chat Bot
+        App
+        <span onClick={toggleTheme} style={{
+        background: "transparent",
+        border: "none",
+        cursor: "pointer",
+        fontSize: "2rem",
+        color: theme === "light" ? "dark" : "light",
+        display: "flex",
+        justifyContent: "end",
+      }}>
+        {theme === "light" ? <FaToggleOff /> : <FaToggleOn />}
+      </span>
+      </h2>
+      <div
+        style={{
+          height: 500,
+          width: 600,
+          overflowY: "scroll",
+          border: "1px solid #ccc",
+          padding: "10px",
+          background: theme === "light" ? "#fff" : "#333",
+          color: theme === "light" ? "#000" : "#fff",
+        }}
+      >
+        {chat.map((msg: any, index:number) => {
+          const showWeekday: any =
+            index === 0 || msg.weekday !== chat[index - 1].weekday;
+          return (
+            <>
+              {showWeekday && <h2 style={{ fontSize: 15 }}>{msg.weekday}</h2>}
+              <div
+                style={{
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent:
+                    msg.type === "user" ? "flex-end" : "flex-start",
+                  marginTop: "10px",
+                  marginBottom: "10px",
+                  color: "white",
+                  padding: "10px",
+                  borderRadius: "10px",
+                }}
+                key={index}
+              >
+
+                {msg.type !== "user" && (
+                  <img
+                    src={chatBotImage}
+                    alt="User Avatar"
+                    style={{
+                      width: 40,
+                      height: 35,
+                      borderRadius: "50%",
+                      marginTop: 1,
+                      marginLeft: -10,
+                    }}
+                  />
+                )}
+
+                {/* <span>{msg.type === "user" ? "YOU" : "BOT"}</span> */}
+                <span
+                  style={{
+                    backgroundColor: msg.type === "user" ? "#e57742" : "gray",
+                    padding: "8px",
+                    marginLeft: 2,
+                    borderRadius: "10px",
+                    borderBottomRightRadius:
+                      msg.type === "user" ? "0px" : "10px",
+                    borderBottomLeftRadius:
+                      msg.type !== "user" ? "0px" : "10px",
+                  }}
+                >
+                  {msg.text}
+                  <span style={{ fontSize: "8px", marginLeft: 20 }}>
+                    {msg.timestamp}
+                  </span>
                 </span>
-                </div>
-            )
-           })}
-        </div>
+                {msg.type === "user" && (
+                  <img
+                    src={personImage}
+                    alt="User Avatar"
+                    style={{
+                      width: 40,
+                      height: 35,
+                      marginTop: 10,
+                      borderRadius: "50%",
+                      marginRight: -10,
+                    }}
+                  />
+                )}
+              </div>
+            </>
+          );
+        })}
+      </div>
 
-        <input type="text" style={{width: 300, padding: 10,margin: 10, marginLeft: -5}} value = {input} onChange={(event) => setinput(event.target.value)} onKeyDown={(e)=> e.key === "Enter" && sendMessage()}/>
-        <button style={{backgroundColor: "lightblue"}} onClick={sendMessage}>Send</button>
-        </>
-    )
-}
+      <div style={{ display: "flex", alignItems: "center" }}>
+        <input
+          type="text"
+          placeholder="Type the message"
+          style={{
+            width: 340,
+            padding: 10,
+            margin: 5,
+            marginLeft: -1,
+            flex: 1, // Allows the input to take available space
+          }}
+          value={input}
+          onChange={(event) => setinput(event.target.value)}
+          onKeyDown={(e) => e.key === "Enter" && sendMessage()}
+        />
+        <span
+          onClick={sendMessage}
+          style={{ cursor: "pointer", marginLeft: 6 }}
+        >
+          <img src={sendIconImg} style={{ height: "40px" }} alt="Send" />
+        </span>
+      </div>
+    </>
+  );
+};
 
 export default ChatApp;
